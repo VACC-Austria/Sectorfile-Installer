1a28be249fcfe4cbed8cec24ec15191959fad54c
Clemens Moritz <6784851+cleboo@users.noreply.github.com>
2025-10-15 21:11:33 +0200

    ensure app data folder exists

--- sectorfile_installer/util/_os.py
+++ sectorfile_installer/util/_os.py
@@ -1,5 +1,8 @@
 import os
 from pathlib import Path
 
+
 def get_app_data_folder() -> Path:
-    return Path(os.getenv("LOCALAPPDATA")) / "SectorfileInstaller"
+    app_data_folder = Path(os.getenv("LOCALAPPDATA")) / "SectorfileInstaller"
+    app_data_folder.mkdir(parents=True, exist_ok=True)
+    return app_data_folder
